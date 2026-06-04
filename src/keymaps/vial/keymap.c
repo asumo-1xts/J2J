186cbef4c6803116b799bea7bec3bb4bdc458982
aSumo <1xtelescope@gmail.com>
2026-06-04 20:47:25 +0900

    feat: `LGUI`と`LALT`を入れ替え

--- src/keymaps/vial/keymap.c
+++ src/keymaps/vial/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,        KC_HOME, 
                 KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,
         KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  KC_END,
-                MO(1),  KC_LALT,KC_LWIN,KC_LNG1,KC_SPC,
+                MO(1),  KC_LWIN,KC_LALT,KC_LNG1,KC_SPC,
                 KC_SPC, KC_LNG2,KC_RWIN,MO(1),  KC_LEFT,        KC_DOWN,KC_RGHT
     ),
     [1] = LAYOUT(
