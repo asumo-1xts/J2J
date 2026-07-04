68cf938c9452e628edb7eb1bcfdcf1f6b9ba3463
aSumo <1xtelescope@gmail.com>
2026-07-04 23:49:22 +0900

    perf: スリープ機能を削除

--- src/keymaps/vial/keymap.c
+++ src/keymaps/vial/keymap.c
@@ -7,10 +7,11 @@
  * ==============================
  */
 
-static uint32_t key_timer;
-bool is_rgb_timeout = false;
+void yawn(void) {
+  uint8_t hue = rgblight_get_hue();
+  uint8_t sat = rgblight_get_sat();
+  uint8_t val = rgblight_get_val();
 
-void yawn(uint8_t hue, uint8_t sat, uint8_t val) {
   for (int16_t i = 0; i <= RGBLIGHT_LIMIT_VAL; i += RGBLIGHT_VAL_STEP) {
     rgblight_sethsv_noeeprom(hue, sat, i);
     wait_ms(30);
@@ -20,26 +21,9 @@ void yawn(uint8_t hue, uint8_t sat, uint8_t val) {
     wait_ms(30);
   }
 
-  rgblight_sethsv_noeeprom(hue, sat, 0);  // いったん消灯
+  rgblight_sethsv_noeeprom(hue, sat, 0);    // いったん消灯
   wait_ms(60);
-  rgblight_sethsv_noeeprom(hue, sat, val);  // 最終的な明るさ
-}
-
-void check_rgb_timeout(void) {
-  if (!is_rgb_timeout && (timer_elapsed32(key_timer) > RGBLIGHT_TIMEOUT)) {
-    is_rgb_timeout = true;
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_TWINKLE);
-    rgblight_suspend();
-  }
-}
-
-void refresh_rgb(void) {
-  key_timer = timer_read32();
-  if (is_rgb_timeout) {
-    is_rgb_timeout = false;
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_wakeup();
-  }
+  rgblight_sethsv_noeeprom(hue, sat, val);  // 点灯
 }
 
 /**
@@ -50,30 +34,20 @@ void refresh_rgb(void) {
 
 void keyboard_post_init_user(void) {
   rgblight_enable_noeeprom();
-  yawn(HUE, SAT, VAL);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+  rgblight_sethsv_noeeprom(HSV_TURQUOISE);
+  yawn();
 }
 
-void housekeeping_task_user(void) {
-#ifdef RGBLIGHT_TIMEOUT
-  check_rgb_timeout();
-#endif
-}
-
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef RGBLIGHT_TIMEOUT
-  if (record->event.pressed) {
-    refresh_rgb();
-  }
-#endif
-}
+// ===============================
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
-    case _MI:  // MIDI layer
-      rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
+    case _MI:
+      rgblight_sethsv_noeeprom(HSV_RED);
       break;
-    default:  // any other layers
-      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    default:
+      rgblight_sethsv_noeeprom(HSV_TURQUOISE);
       break;
   }
   return state;
